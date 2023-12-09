c7bacd30879ec92b3ca0e2128d01c69c86920532
Dylan Brasseur <helios77@outlook.fr>
2023-12-10 00:57:38 +0100

    Day 04

--- src/rust/2023/aoc_common/src/utils.rs
+++ src/rust/2023/aoc_common/src/utils.rs
@@ -20,3 +20,53 @@ pub fn get_roots(a: f64, b: f64, c: f64) -> Option<(f64, f64)> {
         _ => Some((-b / (2.0 * a), -b / (2.0 * a)))
     }
 }
+
+pub trait StringManipulation{
+    fn extract_numbers<'a>(&'a self) -> Box<dyn Iterator<Item=i64> + 'a>;
+    fn split_in<const N: usize>(&self, pat: &str) -> [&str; N];
+
+    fn right_of(&self, pat: &str) -> &str;
+
+    fn left_of(&self, pat: &str) -> &str;
+}
+
+impl StringManipulation for &str {
+    fn extract_numbers<'a>(&'a self) -> Box<dyn Iterator<Item=i64> + 'a> {
+        Box::new(self.split_whitespace().filter_map(|s| s.parse::<i64>().ok()))
+    }
+
+    fn split_in<const N: usize>(&self, pat: &str) -> [&str; N] {
+        self.split(pat).collect::<Vec<&str>>().try_into().unwrap_or_else(|v: Vec<&str>| panic!("Input cannot be split in exactly {} parts. Was split in {} parts", N, v.len()))
+    }
+
+    fn right_of(&self, pat: &str) -> &str {
+        self.split_in::<2>(pat)[1]
+    }
+
+    fn left_of(&self, pat: &str) -> &str {
+        self.split_in::<2>(pat)[0]
+    }
+}
+
+pub fn find_matching_in_sorted<T: Ord>(to_find: &Vec<T>, in_this_list: &Vec<T>) -> usize{
+    let mut j=0;
+    let mut total=0;
+    for x in to_find{
+        while j < in_this_list.len(){
+            match in_this_list.get(j) {
+                None => {panic!("How ?")}
+                Some(y) => {
+                    if y == x{
+                        total+=1;
+                    }
+                    if y >= x{
+                        break;
+                    }
+                    j+=1;
+                }
+            }
+        }
+    }
+
+    total
+}
--- src/rust/2023/day_04/src/lib.rs
+++ src/rust/2023/day_04/src/lib.rs
@@ -1,11 +1,43 @@
+use aoc_common::utils::{find_matching_in_sorted, StringManipulation};
+
+
 #[allow(unused_variables)]
 pub fn solve_1(input: &str) -> String {
-    todo!()
+    let mut total = 0;
+    for [w, h] in input.lines().map(|s| s.right_of(":").split_in::<2>("|").map::<fn(&str) -> Vec<i64>, Vec<i64>>(|s| {
+        let mut v = s.extract_numbers().collect::<Vec<i64>>();
+        v.sort_unstable();
+        v
+    })){
+        match find_matching_in_sorted(&h, &w){
+            0 =>{}
+            x => {total += 2u32.pow((x-1) as u32);}
+        }
+
+    }
+
+    format!("{}", total)
 }
 
 #[allow(unused_variables)]
 pub fn solve_2(input: &str) -> String {
-    todo!()
+    let mut total=0;
+    let originals = input.lines().map(|s| s.right_of(":").split_in::<2>("|").map::<fn(&str) -> Vec<i64>, Vec<i64>>(|s| {
+        let mut v = s.extract_numbers().collect::<Vec<i64>>();
+        v.sort_unstable();
+        v
+    })).collect::<Vec<[Vec<i64>; 2]>>();
+    let mut amount = vec![1i64; originals.len()];
+    for (i, [w, h]) in originals.iter().enumerate(){
+        let cur_amount = *amount.get_mut(i).unwrap();
+        let matches = find_matching_in_sorted(&h, &w);
+        for extra in 1..(matches+1){
+            *amount.get_mut(i+extra).unwrap()+=cur_amount;
+        }
+        total+=cur_amount;
+    }
+
+    format!("{}", total)
 }
 
 #[cfg(test)]
@@ -19,15 +51,15 @@ Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
 
-    const TEST_INPUT_2: &str = r"";
+    const TEST_INPUT_2: &str = TEST_INPUT_1;
 
     #[test]
     fn solves_1() {
-        assert_eq!(solve_1(TEST_INPUT_1), "")
+        assert_eq!(solve_1(TEST_INPUT_1), "13")
     }
 
     #[test]
     fn solves_2() {
-        assert_eq!(solve_2(TEST_INPUT_2), "")
+        assert_eq!(solve_2(TEST_INPUT_2), "30")
     }
 }
