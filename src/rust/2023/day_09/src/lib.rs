8f291280fa4609ebb5c1d45be4acb82675a429c6
Dylan Brasseur <helios77@outlook.fr>
2023-12-16 21:22:33 +0100

    Day 9

--- src/rust/2023/day_09/src/lib.rs
+++ src/rust/2023/day_09/src/lib.rs
@@ -1,11 +1,36 @@
-use aoc_common::aoc_utils::not_yet;
+use aoc_common::string_utils::StringManipulation;
+use itertools::Itertools;
+
+pub fn get_next(vec: Vec<i64>) -> i64{
+    if vec.iter().all_equal(){
+        *vec.last().unwrap()
+    }else {
+        let mut next: Vec<i64> = Vec::with_capacity(vec.len()-1);
+        for i in 0..vec.len()-1{
+            next.push(vec[i+1]-vec[i]);
+        }
+        return vec.last().unwrap()+get_next(next);
+    }
+}
 
 pub fn solve_1(input: &str) -> String {
-    not_yet(input)
+    let lines = input.lines().map(|s| s.extract_numbers().collect_vec());
+    let mut total = 0;
+    for l in lines{
+        total+=get_next(l);
+    }
+
+    format!("{}", total)
 }
 
 pub fn solve_2(input: &str) -> String {
-    not_yet(input)
+    let lines = input.lines().map(|s| {let mut v = s.extract_numbers().collect_vec(); v.reverse(); v});
+    let mut total = 0;
+    for l in lines{
+        total+=get_next(l);
+    }
+
+    format!("{}", total)
 }
 
 #[cfg(test)]
@@ -25,6 +50,6 @@ mod tests {
 
     #[test]
     fn solves_2() {
-        assert_eq!(solve_2(TEST_INPUT_2), "")
+        assert_eq!(solve_2(TEST_INPUT_2), "2")
     }
 }
